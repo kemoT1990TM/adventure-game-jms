b68485ca92ef3a0db94d88f027c7731f2923d218
kemoT1990TM <kartasinski.tomasz@gmail.com>
2021-05-06 13:09:23 +0200

    initial commit - created project

---
+++ src/main/java/com/tkjavadev/adventuregamejms/AdventureGameJmsApplication.java
@@ -0,0 +1,13 @@
+package com.tkjavadev.adventuregamejms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureGameJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureGameJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tkjavadev/adventuregamejms/AdventureGameJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkjavadev.adventuregamejms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureGameJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
